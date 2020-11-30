adf5bad449945a47cace5569e919ac326a63d908
Mary Esekhina <bukva_zu@list.ru>
2020-11-30 20:36:58 +0300

    Lab2

---
+++ DZ_2_Nasledovanie/ClassNasled/Animal.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassNasled
+{
+    abstract class Animal
+    {
+        public int age;
+        public int legs;
+
+        abstract public void AMove();
+
+        public Animal( int age, int legs)
+        {
+            this.age = age;
+            this.legs = legs;
+        }
+
+        public virtual void PrintInfo()
+        {
+            Console.WriteLine("Информация о животном");
+        }
+
+    }
+}
---
+++ DZ_2_Nasledovanie/ClassNasled/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ClassNasled
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog dog1 = new Dog("Шарик", 2, 4, "Овчарка");
+            Dog dog2 = new Dog("Филарин", 1, 4, "Спаниель");
+            Shark SharkT = new Shark("Тигровая", 15, 0, 256);
+            Shark SharkK = new Shark("Китовая", 10, 0, 12546);
+            dog1.AMove();
+            dog2.PrintInfo();
+            SharkT.AMove();
+            SharkK.PrintInfo();
+        }
+    }
+}
