56d5b2440717fe0345142fcb804b152fc39a1266
Mary Esekhina <bukva_zu@list.ru>
2020-12-02 22:09:34 +0300

    DZ4

---
+++ DZ4_Collection/Cloths.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DZ4_Collection
+{
+    class Cloths
+    {
+        public string type;
+        public int size;
+        public Cloths(string type, int size)
+        {
+            this.size = size;
+            this.type = type;
+        }
+
+        public override string ToString()
+        {
+            return String.Format("{0} {1}", type, size);
+        }
+    }
+}
