0bd03e8c89346a58edcf0e7492d5da8a71c4b640
Mary Esekhina <bukva_zu@list.ru>
2020-12-02 22:06:44 +0300

    DZ3

---
+++ DZ_3_Pereopred/ClassNasled/Dog.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassNasled
+{
+    class Dog : Animal
+    {
+        string breed;
+        string name;
+
+        public override void AMove()
+        {
+            Console.WriteLine("Собака бегает");
+        }
+
+        public Dog(string name, int age, int legs, string breed) : base(age, legs)
+        {
+            this.breed = breed;
+            this.name = name;
+        }
+
+        public override void PrintInfo()
+        {
+            Console.WriteLine("Собаку зовут {0}, ей {1} лет, её порода {2}", name, age, breed);
+        }
+
+
+        // Переопределение методов Tostring и Equals
+        public override string ToString()
+        {
+            return String.Format("Собака, имя {0}, возраст {1}, порода {2}, количество лап {3} ", this.name, this.age, this.breed, this.legs);
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Dog dog &&
+                   age == dog.age &&
+                   legs == dog.legs &&
+                   breed == dog.breed &&
+                   name == dog.name;
+        }
+    }
+}
